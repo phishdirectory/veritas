cb1c69af9263e4e7c8ec75747cc6f0edd8e611de
Jasper Mayone <jasper.mayone@phish.directory>
2025-05-03 18:50:45 -0400

    Update statsd.rb
    Signed-off-by: Jasper Mayone <jasper.mayone@phish.directory>

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # StatsD config here
   ENV["STATSD_ENV"] = "production" # This won't send data unless set to production
   ENV["STATSD_ADDR"] = "telemetry.hogwarts.dev:8125" # This is the address of the StatsD server
-  ENV["STATSD_PREFIX"] = "#{Rails.env}.#{Rails.application.class.module_parent_name.downcase}" # This is the prefix for the StatsD metrics
+  ENV["STATSD_PREFIX"] = "#{Rails.env}.phishdirectory.#{Rails.application.class.module_parent_name.downcase}" # This is the prefix for the StatsD metrics
 
   StatsD::Instrument::Environment.setup
 
