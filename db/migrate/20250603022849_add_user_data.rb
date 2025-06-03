e11f4eadc25add0bb1fb984f860d2306aab38b58
Jasper Mayone <jasper.mayone@phish.directory>
2025-06-02 22:34:27 -0400

    catch schema migrations

--- db/migrate/20250603022849_add_user_data.rb
+++ db/migrate/20250603022849_add_user_data.rb
@@ -1,7 +1,7 @@
 class AddUserData < ActiveRecord::Migration[8.0]
   def change
     # todo: implement signup_service
-    add_column :users, :signup_service, :integer, null: false
+    add_column :users, :signup_service, :integer
     # todo: implement services_used
     add_column :users, :services_used, :integer, array: true, default: []
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_02_153223) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_03_022849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_catalog.plpgsql"
@@ -362,6 +362,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_02_153223) do
     t.datetime "updated_at", null: false
     t.boolean "staff", default: false, null: false
     t.boolean "pd_dev", default: false, null: false
+    t.integer "signup_service"
+    t.integer "services_used", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["pd_id"], name: "index_users_on_pd_id", unique: true
   end
