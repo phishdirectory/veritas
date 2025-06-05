8196f5c86926d4b4523626aadaa778125ab2509e
Jasper Mayone <jasper.mayone@phish.directory>
2025-06-05 11:44:23 -0400

    heartbeat stability

--- app/jobs/heartbeat_job.rb
+++ app/jobs/heartbeat_job.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class HeartbeatJob < ApplicationJob
-  def perform(*args)
-    # Do something later
-    StatsD.increment("heartbeat")
+  def perform(expected_queue = nil)
+    queue = expected_queue.presence || self.queue_name.presence || "unknown"
+
+    # Send metric with queue in the name instead of as a tag
+    StatsD.increment("heartbeat.#{queue}")
   end
 
 end
