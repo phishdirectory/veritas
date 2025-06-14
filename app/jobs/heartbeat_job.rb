13abb6394b8d3b80683cd3928d479e4d238a07e5
Jasper Mayone <jasper.mayone@phish.directory>
2025-06-14 14:05:05 -0400

    dont run heartbeat job in prod

--- app/jobs/heartbeat_job.rb
+++ app/jobs/heartbeat_job.rb
@@ -4,8 +4,13 @@ class HeartbeatJob < ApplicationJob
   def perform(expected_queue = nil)
     queue = expected_queue.presence || self.queue_name.presence || "unknown"
 
+    # if rails env is not development
+    return unless Rails.env.development?
+
+
     # Send metric with queue in the name instead of as a tag
     StatsD.increment("heartbeat.#{queue}")
+
   end
 
 end
