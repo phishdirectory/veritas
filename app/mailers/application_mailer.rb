0f605d8f3e32e564b173c3d7496d85d903db636a
Jasper Mayone <jasper.mayone@phish.directory>
2025-06-15 15:51:43 -0400

    fix errors

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  include Rails.application.routes.url_helpers
   has_history
   utm_params
   default from: "no-reply@transactional.phish.directory"
