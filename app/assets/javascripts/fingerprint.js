ff91e5a73889494e1028c05a281fdeba91b3228b
Jasper Mayone <jasper.mayone@phish.directory>
2025-06-15 10:37:00 -0400

    fix some bugs

--- app/assets/javascripts/fingerprint.js
+++ app/assets/javascripts/fingerprint.js
@@ -1,19 +1,36 @@
-; (async () => {
-  const csrfTokenSelector = document.querySelector('[name=csrf-token]')
-  let token = ''
-  if (csrfTokenSelector && csrfTokenSelector.content) {
-    // eslint-disable-next-line no-unused-vars
-    token = csrfTokenSelector.content
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    // Use the globally loaded FingerprintJS
+    if (window.FingerprintJS) {
+      const fp = await window.FingerprintJS.load()
+      const result = await fp.get()
+      const visitorId = result.visitorId
+
+      const fingerprintField = document.querySelector('#fingerprint')
+      if (fingerprintField) {
+        fingerprintField.value = visitorId
+      }
+
+      const deviceField = document.querySelector('#device_info')
+      if (deviceField && window.UAParser) {
+        const parser = new UAParser()
+        const browser = parser.getBrowser()
+        deviceField.value = `${browser.name} ${browser.version}`
+      }
+
+      const osField = document.querySelector('#os_info')
+      if (osField && window.UAParser) {
+        const parser = new UAParser()
+        const os = parser.getOS()
+        osField.value = `${os.name} ${os.version || ''}`
+      }
+
+      const timezoneField = document.querySelector('#timezone')
+      if (timezoneField) {
+        timezoneField.value = Intl.DateTimeFormat().resolvedOptions().timeZone
+      }
+    }
+  } catch (error) {
+    console.error('Error collecting fingerprint:', error)
   }
-
-  const fp = await import('https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3.X/+esm').then(
-    FingerprintJS => FingerprintJS.load()
-  )
-  const result = await fp.get()
-  const visitorId = result.visitorId
-
-  document.querySelector('#fingerprint').value = visitorId
-  document.querySelector('#device_info').value = $.ua.browser.name + ' ' + $.ua.browser.version
-  document.querySelector('#os_info').value = $.ua.os.name + ($.ua.os.version ? ' ' + $.ua.os.version : '')
-  document.querySelector('#timezone').value = result.components.timezone.value
-})()
+})
